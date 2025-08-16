16a100f6f83b861fa3130250c88fc02238aff099
Martina Scholz <martina@simplysmart-it.de>
2025-08-16 17:19:09 +0200

    Fix apply id  on new items for historyentry

--- libraries/src/MVC/Model/AdminModel.php
+++ libraries/src/MVC/Model/AdminModel.php
@@ -1451,6 +1451,9 @@ abstract class AdminModel extends FormModel
 
             $historyData = array_merge($tableData, $data);
 
+            // We have to set the key for new items, would be always 0 otherwise
+            $historyData[$key] = $this->getState($this->getName() . '.id');
+
             $this->saveHistory($historyData, $context);
         }
 
